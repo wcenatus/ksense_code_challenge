6bc8cbbb87ff2d5af613f3bee3b1ef5341cc2b6a
wcenatus <wcenatus@gmail.com>
2025-03-06 02:32:35 -0500

    Update format for get request and add interface for payload

--- src/controllers/payload.ts
+++ src/controllers/payload.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
 import { db } from "../db/db";
 
+interface Payload {
+  id: number;
+  header: string;
+  body: string;
+  query: string;
+}
+
 export const create = async (req: Request, res: Response) => {
   console.log("Payload:", req);
   //Here I'm capturing relavant data in from the request because we
@@ -26,12 +33,19 @@ export const create = async (req: Request, res: Response) => {
 //Route for Getting all payload data (extra)
 export const getAll = async (req: Request, res: Response) => {
   const dbquery = `SELECT * FROM payload`;
-  db.all(dbquery, (err, rows) => {
+  db.all(dbquery, (err, rows: Payload[]) => {
     if (err) {
       console.error("Select Error:", err);
       res.sendStatus(500);
     } else {
-      res.json(rows);
+      res.json(
+        rows.map((row) => ({
+          id: row.id,
+          body: JSON.parse(row.body),
+          header: JSON.parse(row.header),
+          query: JSON.parse(row.query),
+        }))
+      );
     }
   });
 };
